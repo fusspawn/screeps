176d752a99ecac1d0a55aa967c387079b313c52a
Paul Thornton <ariochofmelnibone@gmail.com>
2016-09-28 18:30:06 +0100

    Cleanslate v3

---
+++ v3/ai_handler.js
@@ -0,0 +1,5 @@
+module.exports = {
+    tick: function() {
+        console.log("tick");
+    }
+}
---
+++ v3/spawner.js
@@ -0,0 +1,5 @@
+module.exports = {
+    tick: function() {
+        MEMORY.SpawnMode = "Manual Only";
+    }
+}
