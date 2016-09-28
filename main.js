3a4b923677810bcace51d5988ed30bd3e1b98201
Paul Thornton <ariochofmelnibone@gmail.com>
2016-09-28 18:31:17 +0100

    fixup import paths

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-var spawner = require("v3/spawner.js");
-var ai_handler = require("v3/ai_handler.js");
+var spawner = require("./v3/spawner.js");
+var ai_handler = require("./v3/ai_handler.js");
 
 spawner.tick();
 ai_handler.tick();
